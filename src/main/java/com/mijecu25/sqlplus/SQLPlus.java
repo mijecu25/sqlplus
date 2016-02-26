1d92d78515edcc192b87146d9df206efb141217c
Miguel Velez <miguelvelez@mijecu25.com>
2016-02-25 21:26:35 -0600

    Add dependencies and sqlplus prompt

--- src/main/java/com/mijecu25/sqlplus/SQLPlus.java
+++ src/main/java/com/mijecu25/sqlplus/SQLPlus.java
@@ -9,6 +9,7 @@ public class SQLPlus {
     private static final String EXIT = "exit";
     private static final String QUIT = "quit";
     private static final int    UILENGTH = 80;
+    private static final String PROMPT = "sqlplus> ";
     
     public static void main(String[] args) {
         // Create a scanner to get user input
@@ -20,11 +21,11 @@ public class SQLPlus {
         System.out.println(StringUtils.center("", SQLPlus.UILENGTH, '-'));
         System.out.println(StringUtils.center(" Type " + SQLPlus.EXIT + " or " + SQLPlus.QUIT + " to exit the application ", SQLPlus.UILENGTH, '-'));
         System.out.println(StringUtils.center("", SQLPlus.UILENGTH, '-'));
-        
+
         // Execute the input scanner
         while(true) {
             // Get a line from the user until the hit enter (carriage return, line feed/ new line)
-            System.out.print("Please enter something: ");
+            System.out.print(SQLPlus.PROMPT);
             String input = inputScanner.nextLine().trim();
             
             // If the user did not enter anything 
@@ -39,12 +40,12 @@ public class SQLPlus {
                 break;
             }
             
+            // TODO this is for test. REMOVE
             System.out.println("You entered: " + input);
         }
         
         // Close the input scanner
         inputScanner.close();
-
     }
-
+    
 }
