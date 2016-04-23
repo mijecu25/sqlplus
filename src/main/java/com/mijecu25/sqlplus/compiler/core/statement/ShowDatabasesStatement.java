093be7b8c996102ec6da2eb38d9aef50f127fa02
Miguel Velez <miguelvelez@mijecu25.com>
2016-04-23 15:08:11 -0500

    Initial commit of Statements

---
+++ src/main/java/com/mijecu25/sqlplus/compiler/core/statement/ShowDatabasesStatement.java
@@ -0,0 +1,18 @@
+package com.mijecu25.sqlplus.compiler.core.statement;
+
+/**
+ * 
+ * @author Miguel Velez - miguelvelezmj25
+ * @version 0.1.0.1  
+ */
+public class ShowDatabasesStatement extends Statement {
+
+    public ShowDatabasesStatement() {
+        ;
+    }
+    
+    @Override
+    public String toString() {
+        return "This is a show databases expression";
+    }
+}
---
+++ src/main/java/com/mijecu25/sqlplus/compiler/core/statement/Statement.java
@@ -0,0 +1,22 @@
+package com.mijecu25.sqlplus.compiler.core.statement;
+
+/**
+ * 
+ * @author Miguel Velez - miguelvelezmj25
+ * @version 0.1.0.1 
+ */
+public abstract class Statement {
+    
+    public Statement() {
+        ;
+    }
+    
+    public void execute() {
+        ;
+    }
+    
+    public void printResult() {
+        ;
+    }
+    
+}
