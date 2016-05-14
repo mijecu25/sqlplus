4423b2c61020a9c4609cc5662aa78660a43f40b7
Miguel Velez <miguelvelez@mijecu25.com>
2016-05-14 12:22:50 -0500

    Handle user interrupt

--- src/main/java/com/mijecu25/sqlplus/SQLPlus.java
+++ src/main/java/com/mijecu25/sqlplus/SQLPlus.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import jline.console.UserInterruptException;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
@@ -28,7 +29,7 @@ import jline.console.ConsoleReader;
  * SQLPlus add alerts to your sql queries.
  * 
  * @author Miguel Velez - miguelvelezmj25
- * @version 0.1.0.12
+ * @version 0.1.0.13
  */
 public class SQLPlus {
 
@@ -48,167 +49,181 @@ public class SQLPlus {
     private static final Logger logger = LogManager.getLogger(SQLPlus.class);
     
     public static void main(String[] args) throws IOException {
-     // Create and load the properties from the application properties file
+        // Create and load the properties from the application properties file
         Properties properties = new Properties();
         properties.load(SQLPlus.class.getClassLoader().getResourceAsStream(SQLPlus.APPLICATION_PROPERTIES_FILE));
-        
-        
+
+
         SQLPlus.logger.info("Initializing " + SQLPlus.PROGRAM_NAME + " version " + properties.getProperty(SQLPlus.APPLICATION_PROPERTIES_FILE_VERSION));
-        
+
         // Check if the user is using a valid console (i.e. not from Eclipse)
-        if(System.console() == null) {
+        if (System.console() == null) {
             // The Console object for the JVM could not be found. Alert the user 
             SQLPlus.logger.fatal(Messages.FATAL + "A JVM Console object was not found. Try running " + SQLPlus.PROGRAM_NAME
                     + "from the command line");
             System.out.println(Messages.FATAL + SQLPlus.PROGRAM_NAME + " was not able to find your JVM's Console object. "
                     + "Try running " + SQLPlus.PROGRAM_NAME + " from the command line.");
-            
+
             SQLPlus.exitSQLPlus();
-          
+
             SQLPlus.logger.fatal(Messages.FATAL + Messages.QUIT_PROGRAM_ERROR(PROGRAM_NAME));
-            return ;
+            return;
         }
-                        
+
         // UI intro
         System.out.println("Welcome to " + SQLPlus.PROGRAM_NAME + "! This program has a DSL to add alerts to various SQL DML events.");
         System.out.println("Be sure to use " + SQLPlus.PROGRAM_NAME + " from the command line.");
         System.out.println();
-        
+
         // Get the version
         System.out.println("Version: " + properties.getProperty(SQLPlus.APPLICATION_PROPERTIES_FILE_VERSION));
         System.out.println();
-        
+
         // Read the license file
         BufferedReader bufferedReader = null;
         bufferedReader = new BufferedReader(new FileReader(SQLPlus.LICENSE_FILE));
 
         // Read a line
         String line = bufferedReader.readLine();
-        
+
         // While the line is not null
         while (line != null) {
             System.out.println(line);
-        
+
             // Read a new lines
             line = bufferedReader.readLine();
         }
 
         // Close the buffer
-        bufferedReader.close();        
+        bufferedReader.close();
         System.out.println();
 
-        // Create the jline console that allows us to remember commands and use arrow keys
+        // Create the jline console that allows us to remember commands, use arrow keys, and catch interruptions
+        // from the user
         SQLPlus.console = new ConsoleReader();
-        
+        SQLPlus.console.setHandleUserInterrupt(true);
+
         try {
             // Get credentials from the user
             SQLPlus.logger.info("Create SQLPlusConnection");
             SQLPlus.createSQLPlusConnection();
-        } 
-        catch(NullPointerException npe) {
+        } catch (NullPointerException npe) {
             // This exception can occur if the user is running the program where the JVM Console
             // object cannot be found
             SQLPlus.logger.fatal(Messages.FATAL + Messages.FATAL_EXIT(SQLPlus.PROGRAM_NAME, npe.getClass().getName()));
             System.out.println(Messages.FATAL + Messages.FATAL_EXCEPTION_ACTION(npe.getClass().getSimpleName())
                     + Messages.SPACE + Messages.CHECK_LOG_FILES);
             SQLPlus.exitSQLPlus();
-            
+
             SQLPlus.logger.fatal(Messages.FATAL + Messages.QUIT_PROGRAM_ERROR(SQLPlus.PROGRAM_NAME));
-            return ;
-        }
-        catch(SQLException sqle) {
+            return;
+        } catch (SQLException sqle) {
             // This exception can occur when trying to establish a connection
             SQLPlus.logger.fatal(Messages.FATAL + Messages.FATAL_EXIT(SQLPlus.PROGRAM_NAME, sqle.getClass().getName()));
             System.out.println(Messages.FATAL + Messages.FATAL_EXCEPTION_ACTION(sqle.getClass().getSimpleName())
                     + Messages.SPACE + Messages.CHECK_LOG_FILES);
             SQLPlus.exitSQLPlus();
-            
+
             SQLPlus.logger.fatal(Messages.FATAL + Messages.QUIT_PROGRAM_ERROR(SQLPlus.PROGRAM_NAME));
-            return ;
-        }
-        catch(IllegalArgumentException iae) {
+            return;
+        } catch (IllegalArgumentException iae) {
             // This exception can occur when trying to establish a connection
             SQLPlus.logger.fatal(Messages.FATAL + Messages.FATAL_EXIT(SQLPlus.PROGRAM_NAME, iae.getClass().getName()));
             System.out.println(Messages.FATAL + Messages.FATAL_EXCEPTION_ACTION(iae.getClass().getSimpleName())
                     + Messages.SPACE + Messages.CHECK_LOG_FILES);
             SQLPlus.exitSQLPlus();
-            
+
             SQLPlus.logger.fatal(Messages.FATAL + Messages.QUIT_PROGRAM_ERROR(SQLPlus.PROGRAM_NAME));
-            return ;
+            return;
         }
-        
+
         System.out.println("Connection established! Commands end with " + SQLPlus.END_OF_COMMAND);
         System.out.println("Type " + SQLPlus.EXIT + " or " + SQLPlus.QUIT + " to exit the application ");
-               
-        // Execute the input scanner
-        while(true) {
-            // Get a line from the user until the hit enter (carriage return, line feed/ new line). 
-            System.out.print(SQLPlus.PROMPT);
-            line = console.readLine().trim();
 
-            // If the user did not enter anything 
-            if(line.isEmpty()) {
-                // Continue to the next iteration
-                continue;
-            }
-            
-            // Logic to quit
-            if(line.equals(SQLPlus.QUIT) || line.equals(SQLPlus.EXIT)) {
-                SQLPlus.logger.info("The user wants to quit " + SQLPlus.PROGRAM_NAME);
-                SQLPlus.exitSQLPlus();
-                break;
-            }      
-            
-            // Use a StringBuilder since jline works weird when it has read a line. The issue we were having was with the 
-            // end of command logic. jline does not keep the input from the user in the variable that was stored in. Each 
-            // time jline reads a new line, the variable is empty 
-            StringBuilder query = new StringBuilder();
-            query.append(line);
-            
-            // While the user does not finish the command with the SQLPlus.END_OF_COMMAND
-            while(query.charAt(query.length()-1) != SQLPlus.END_OF_COMMAND) {
-                // Print the wait for command prompt and get the next line for the user
-                System.out.print(SQLPlus.WAIT_FOR_END_OF_COMMAND);
-                query.append(" ");
-                line = StringUtils.stripEnd(console.readLine(), null);
+        try {
+            // Execute the input scanner
+            while (true) {
+                // Get a line from the user until the hit enter (carriage return, line feed/ new line).
+                System.out.print(SQLPlus.PROMPT);
+                try {
+                    line = SQLPlus.console.readLine().trim();
+                }
+                catch(NullPointerException npe) {
+                    // If this exception is catch, it is very likely that the user entered the end of line command.
+                    // This means that the program should quit.
+                    SQLPlus.logger.warn(Messages.WARNING + "The input from the user is null. It is very likely that" +
+                            "the user entered the end of line command and they want to quit.");
+                    SQLPlus.exitSQLPlus();
+
+                    return;
+                }
+                // If the user did not enter anything
+                if (line.isEmpty()) {
+                    // Continue to the next iteration
+                    continue;
+                }
+
+                // Logic to quit
+                if (line.equals(SQLPlus.QUIT) || line.equals(SQLPlus.EXIT)) {
+                    SQLPlus.logger.info("The user wants to quit " + SQLPlus.PROGRAM_NAME);
+                    SQLPlus.exitSQLPlus();
+                    break;
+                }
+
+                // Use a StringBuilder since jline works weird when it has read a line. The issue we were having was with the
+                // end of command logic. jline does not keep the input from the user in the variable that was stored in. Each
+                // time jline reads a new line, the variable is empty
+                StringBuilder query = new StringBuilder();
                 query.append(line);
-            }
 
-            SQLPlus.logger.info("Raw input from the user: " + query);
-            try {
+                // While the user does not finish the command with the SQLPlus.END_OF_COMMAND
+                while (query.charAt(query.length() - 1) != SQLPlus.END_OF_COMMAND) {
+                    // Print the wait for command prompt and get the next line for the user
+                    System.out.print(SQLPlus.WAIT_FOR_END_OF_COMMAND);
+                    query.append(" ");
+                    line = StringUtils.stripEnd(SQLPlus.console.readLine(), null);
+                    query.append(line);
+                }
+
+                SQLPlus.logger.info("Raw input from the user: " + query);
+
                 // Execute the antlr code to parse the user input
                 SQLPlus.logger.info("Will parse the user input to determine what to execute");
                 ANTLRStringStream input = new ANTLRStringStream(query.toString());
                 SQLPlusLex lexer = new SQLPlusLex(input);
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
                 SQLPlusParser parser = new SQLPlusParser(tokens);
-                
+
                 // TODO handle this better so that I print my own message when the string is not matched
                 Statement statement = parser.sqlplus();
-                
-                if(statement == null) {
+
+                if (statement == null) {
                     System.out.println("Return value is null");
-                }
-                else {
+                } else {
                     SQLPlus.sqlPlusConnection.execute(statement);
                 }
-            } catch (RecognitionException e) {
-                // TODO test this
-                SQLPlus.logger.warn("There was an error while parsing the user input");
-                e.printStackTrace();
             }
-            catch(IllegalArgumentException iae) {
-                // This exception can occur when a command is executed but it had illegal arguments. Most likely
-                // it is a programmer's error and should be addressed by the developer.
-                SQLPlus.logger.fatal(Messages.FATAL + Messages.FATAL_EXIT(SQLPlus.PROGRAM_NAME, iae.getClass().getName()));
-                SQLPlus.exitSQLPlus();
-
-                SQLPlus.logger.fatal(Messages.FATAL + Messages.QUIT_PROGRAM_ERROR(SQLPlus.PROGRAM_NAME));
-                return ;
-            }
-            
         }
-        
+        catch(RecognitionException e){
+            // TODO test this
+            SQLPlus.logger.warn("There was an error while parsing the user input");
+            e.printStackTrace();
+        }
+        catch(IllegalArgumentException iae){
+            // This exception can occur when a command is executed but it had illegal arguments. Most likely
+            // it is a programmer's error and should be addressed by the developer.
+            SQLPlus.logger.fatal(Messages.FATAL + Messages.FATAL_EXIT(SQLPlus.PROGRAM_NAME, iae.getClass().getName()));
+            SQLPlus.exitSQLPlus();
+
+            SQLPlus.logger.fatal(Messages.FATAL + Messages.QUIT_PROGRAM_ERROR(SQLPlus.PROGRAM_NAME));
+            return ;
+        }
+        catch(UserInterruptException uie) {
+            SQLPlus.logger.warn(Messages.WARNING + "The user typed an interrupt instruction.");
+            SQLPlus.exitSQLPlus();
+
+            return ;
+        }
     }
     
     /**
@@ -224,7 +239,7 @@ public class SQLPlus {
 
         // Add credentials
         System.out.print(SQLPlus.PROMPT + "Host(default " + SQLPlusConnection.getDefaultHost() + "): ");
-        String host = console.readLine().trim();
+        String host = SQLPlus.console.readLine().trim();
         SQLPlus.logger.info("User entered host:" + host);
         
         // TODO remove this later when other hosts are supported
@@ -240,7 +255,7 @@ public class SQLPlus {
         }
         
         System.out.print(SQLPlus.PROMPT + "Database(default " + SQLPlusConnection.getDefaultDatabase() + "): ");
-        String database = console.readLine().trim();
+        String database = SQLPlus.console.readLine().trim();
         SQLPlus.logger.info("User entered database:" + database);
       
         String port = "";
@@ -248,7 +263,7 @@ public class SQLPlus {
         // While the port is not numeric
         while(!StringUtils.isNumeric(port)) {
             System.out.print(SQLPlus.PROMPT + "Port (default " + SQLPlusConnection.getDefaultPort() + "): ");
-            port = console.readLine().trim();
+            port = SQLPlus.console.readLine().trim();
             SQLPlus.logger.info("Port entered: " + port);
             SQLPlus.logger.info("Port string length: " + port.length());
             
@@ -275,7 +290,7 @@ public class SQLPlus {
         // While the username is empty
         while(username.isEmpty()) {
             System.out.print(SQLPlus.PROMPT + "Username: ");
-            username = console.readLine().trim();
+            username = SQLPlus.console.readLine().trim();
             
             // If the username is empty
             if(username.isEmpty()) {
