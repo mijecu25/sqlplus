663cf04a916db699030571e02730907a535da2f2
Miguel Velez <miguelvelez@mijecu25.com>
2016-07-12 18:36:18 -0500

    Add methods to abstract and generalize behavior

--- src/main/java/com/mijecu25/sqlplus/compiler/core/statement/dml/StatementDML.java
+++ src/main/java/com/mijecu25/sqlplus/compiler/core/statement/dml/StatementDML.java
@@ -1,6 +1,7 @@
 package com.mijecu25.sqlplus.compiler.core.statement.dml;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -14,7 +15,7 @@ import com.mijecu25.sqlplus.compiler.core.statement.Statement;
  * This class contains common behavior for all DML SQL statements.
  *
  * @author Miguel Velez - miguelvelezmj25
- * @version 0.1.0.7
+ * @version 0.1.0.8
  */
 public abstract class StatementDML extends Statement {
     private static final Logger logger = LogManager.getLogger(StatementDML.class);
@@ -29,7 +30,7 @@ public abstract class StatementDML extends Statement {
         if(tables.size() > 1) {
             // We currently support only 1 table reference
             UnsupportedOperationException uoe = new UnsupportedOperationException();
-            String message = Messages.WARNING + "You can only enter a single table reference in the FROM clause";
+            String message = Messages.WARNING + "You can only enter a single table reference";
             StatementDML.logger.warn(message);
             System.out.println(message);
             StatementDML.logger.warn(Messages.WARNING + "Throwing a " + uoe.getClass().getSimpleName() + " to the calling class");
@@ -47,10 +48,87 @@ public abstract class StatementDML extends Statement {
     protected abstract void printResult();
 
     /**
+     * TODO
+     *
+     * @param connection the connection used to execute the query.
+     * @throws SQLException if there is a problem executing the query.
+     */
+    protected void executeUpdate(Connection connection) throws SQLException {
+        if(connection == null) {
+            IllegalArgumentException iae = new IllegalArgumentException();
+            StatementDML.logger.fatal(Messages.FATAL + "The connection passed to execute the query "
+                    + "cannot be null");
+            System.out.println(Messages.FATAL + Messages.FATAL_EXCEPTION_ACTION(iae.getClass().getSimpleName()) + " "
+                    + Messages.CHECK_LOG_FILES);
+            StatementDML.logger.warn(Messages.WARNING + "Throwing a " + iae.getClass().getSimpleName()
+                    + " to the calling class");
+            throw iae;
+        }
+
+        try {
+            // Create a Java statement with a resultSet that can be scrolled and cannot be updated while scrolling
+            java.sql.Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+            long startTime = System.nanoTime();
+            int rowCount = statement.executeUpdate(this.statement);
+            long endTime = System.nanoTime();
+
+            System.out.println(StatementDML.displayNumberOfAffectedRows(rowCount));
+
+            System.out.printf("Execution time: %.2f sec\n", (endTime - startTime)/1000000000.0);
+
+            statement.close();
+        }
+        catch(SQLException sqle) {
+            StatementDML.logger.warn(Messages.WARNING + "Error when executing " + this, sqle);
+            System.out.println(Messages.WARNING + "(" + sqle.getErrorCode() + ") (" + sqle.getSQLState() + ") "
+                    + sqle.getMessage());
+
+            StatementDML.logger.warn(Messages.WARNING + "Throwing a " + sqle.getClass().getSimpleName()
+                    + " to the calling class");
+            throw sqle;
+        }
+    }
+
+    /**
      * Helper method to return the first table in the list of referenced tables.
      *
      * @return a string with the name of the first referenced table.
      */
     public String getFirstTable() { return this.tables.get(0); }
+
+    /**
+     * Format a string that displays the number of rows affected by a query.
+     *
+     * @param rowCount the number of rows affected by a query.
+     *
+     * @return a string formatted based on the numbers of affected rows.
+     */
+    public static String displayNumberOfAffectedRows(int rowCount) {
+        if(rowCount < 0) {
+            IllegalArgumentException iae = new IllegalArgumentException();
+            StatementDML.logger.fatal(Messages.FATAL + "The minimum number of affected rows: " + rowCount
+                    + " cannot be less than 0");
+            System.out.println(Messages.FATAL + Messages.FATAL_EXCEPTION_ACTION(iae.getClass().getSimpleName())
+                    + " " + Messages.CHECK_LOG_FILES);
+            StatementDML.logger.warn(Messages.WARNING + "Throwing a " + iae.getClass().getSimpleName() + " to the calling class");
+            throw iae;
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append("Query OK, ");
+        result.append(rowCount);
+
+        if(rowCount == 1) {
+            result.append(" row");
+        }
+        else {
+            result.append(" rows");
+        }
+
+        result.append(" in set");
+
+        return result.toString();
+    }
 
 }
--- src/main/java/com/mijecu25/sqlplus/compiler/core/statement/dml/StatementInsertStatement1.java
+++ src/main/java/com/mijecu25/sqlplus/compiler/core/statement/dml/StatementInsertStatement1.java
@@ -1,29 +1,51 @@
 package com.mijecu25.sqlplus.compiler.core.statement.dml;
 
+import com.mijecu25.messages.Messages;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * TODO
+ * This class represents the "insert...." SQL statement. It inserts rows into a table.
  *
  * @author Miguel Velez - miguelvelezmj25
  * @version 0.1.0.1
  */
 public class StatementInsertStatement1 extends StatementDML {
-
     private List<List<String>> valuesList;
+
+    private static final Logger logger = LogManager.getLogger(StatementSelectExpression.class);
 
     public StatementInsertStatement1(String table, List<String> columns, List<List<String>> valuesList) {
         super(columns, StatementInsertStatement1.tableToList(table));
 
         this.valuesList = valuesList;
+
+        StatementInsertStatement1.logger.info("Parsed and created a StatementInsertStatement1");
     }
 
     @Override
     public void execute(Connection connection) throws SQLException {
+        StatementInsertStatement1.logger.info("Will execute the code to query the database using a select statement");
 
+        if(connection == null) {
+            IllegalArgumentException iae = new IllegalArgumentException();
+            StatementInsertStatement1.logger.fatal(Messages.FATAL + "The connection passed to execute the statement "
+                    + "cannot be null");
+            System.out.println(Messages.FATAL + Messages.FATAL_EXCEPTION_ACTION(iae.getClass().getSimpleName()) + " "
+                    + Messages.CHECK_LOG_FILES);
+            StatementInsertStatement1.logger.warn(Messages.WARNING + "Throwing a " + iae.getClass().getSimpleName()
+                    + " to the calling class");
+            throw iae;
+        }
+
+        // TODO this is where the sqlplus alert should check before or after this insert statement
+        this.executeUpdate(connection);
+        // TODO Do we need to catch the exception here?
     }
 
     @Override
